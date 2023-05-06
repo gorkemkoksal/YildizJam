06323940eee9a2039a03679148dbf91229e2c632
gorkemkoksal <81961582+gorkemkoksal@users.noreply.github.com>
2023-05-06 23:46:15 +0300

    1

--- YildizJam/Assets/Scripts/GroundChecker.cs
+++ YildizJam/Assets/Scripts/GroundChecker.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+
 
 public class GroundChecker : MonoBehaviour
 {
     public bool IsGrounded=true;
+    private bool isStillFalling;
+    public float platformHeight;
+    public event Action OnFalling;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Surface"))
         {
             IsGrounded = true;
+            platformHeight = collision.transform.position.y;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Surface"))
+        {
+            IsGrounded = false;         
+        }
+    }
+    private void Update()
+    {
+        if (platformHeight - transform.position.y > 0.5f && !IsGrounded && !isStillFalling)
+        {
+            isStillFalling = true;
+            OnFalling?.Invoke();
         }
     }
 }
---
+++ YildizJam/Assets/Scripts/States/PlayerStates/FallingState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingState : PlayerBaseState
+{
+    private bool isSecondJump;
+    public FallingState(PlayerStateMachine stateMachine, int characterIndex) : base(stateMachine, characterIndex)
+    {
+    }
+    public override void Enter()
+    {
+    }
+    public override void Exit()
+    {
+    }
+    public override void FixedTick(float fixedDeltatime)
+    {
+        Move(Input.GetAxis("Horizontal"), characterIndex);
+    }
+    public override void Tick(float deltaTime)
+    {
+        if (stateMachine.GroundChecker.IsGrounded)
+        {
+            stateMachine.SwitchState(new RunState(stateMachine, characterIndex));
+        }
+        if (characterIndex == 0 && !isSecondJump && Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(characterIndex);
+        }
+    }
+    public override void OnTriggerEnter2D(Collider2D other)
+    {
+        base.OnTriggerEnter2D(other);
+    }
+}
